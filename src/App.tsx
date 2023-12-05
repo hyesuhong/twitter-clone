9948969948d8edaaea6b8453f8f5dfa8e8efe976
Hyesu <honghs95@gmail.com>
2023-12-05 18:55:34 +0900

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import { RouterProvider } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import router from './Router';
+
+const GlobalStyle = createGlobalStyle`
+	${reset}
+
+	* {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: black;
+    color:white;
+    font-family:'Raleway', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		height: 100vh;
+		min-height: 100vh;
+
+		& > div {
+			display: flex;
+			justify-content: center;
+			height: 100%;
+		}
+  }
+`;
 
 function App() {
 	return (
 		<>
+			<GlobalStyle />
 			<RouterProvider router={router} />
 		</>
 	);
