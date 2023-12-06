5e40ff34080a83c6d68bddb24ddad1fb05a3ca53
Hyesu <honghs95@gmail.com>
2023-12-06 19:33:42 +0900

    feat: change basic layout

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,9 +1,41 @@
 import { Outlet } from 'react-router-dom';
+import SideMenu from '../components/SideMenu';
+import styled from 'styled-components';
+import SideSearch from '../components/SideSearch';
+import SideProfile from '../components/SideProfile';
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	gap: 20px;
+	width: min(100%, 1400px);
+	padding: 32px 20px;
+
+	& > main {
+		flex: 1;
+	}
+`;
+
+const RightSide = styled.aside`
+	display: grid;
+	grid-template-rows: 1fr max-content;
+	gap: 32px;
+	width: 16.49vw;
+`;
 
 const BasicLayout = () => {
 	return (
 		<>
-			<Outlet />
+			<Wrapper>
+				<SideMenu />
+				<main>
+					<Outlet />
+				</main>
+				<RightSide>
+					<SideSearch />
+					<SideProfile />
+				</RightSide>
+			</Wrapper>
 		</>
 	);
 };
