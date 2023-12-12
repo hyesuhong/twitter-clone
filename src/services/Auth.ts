f94b4bdd9ebb0a2e00f653548c4abd068b81b732
Hyesu <honghs95@gmail.com>
2023-12-12 19:29:57 +0900

    feat: store user's some data in a db

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -9,6 +9,8 @@ import {
 import { FirebaseError } from 'firebase/app';
 import { authUserInfo, joinUserInfo, socialType } from '../types/auth';
 import { errorMsg } from '../constants/message';
+import { db } from '../firebase';
+import { addDoc, collection, getDocs, query, where } from '@firebase/firestore';
 
 export default class AuthService {
 	readonly auth: Auth;
@@ -24,18 +26,38 @@ export default class AuthService {
 			password
 		);
 
-		await updateProfile(credentials.user, { displayName: name });
+		const { user } = credentials;
+
+		await updateProfile(user, { displayName: name });
+
+		await this.writeUserData({ name, uid: user.uid, email: user.email });
 	}
 
 	async loginWithEmail({ email, password }: authUserInfo) {
-		await signInWithEmailAndPassword(this.auth, email, password);
+		const credentials = await signInWithEmailAndPassword(
+			this.auth,
+			email,
+			password
+		);
+		const {
+			user: { displayName, uid, email: uEmail },
+		} = credentials;
+
+		await this.writeUserData({ name: displayName, uid: uid, email: uEmail });
 	}
 
 	async loginWithSocial(type: socialType) {
 		switch (type) {
 			case 'GITHUB': {
 				const provider = new GithubAuthProvider();
-				await signInWithPopup(this.auth, provider);
+				const credentials = await signInWithPopup(this.auth, provider);
+
+				const {
+					user: { displayName, uid, email },
+				} = credentials;
+
+				await this.writeUserData({ name: displayName, uid: uid, email: email });
+
 				break;
 			}
 		}
@@ -48,5 +70,28 @@ export default class AuthService {
 			const e = error as Error;
 			return e.message;
 		}
+	}
+
+	private async writeUserData({
+		uid,
+		name,
+		email = '',
+	}: {
+		uid: string;
+		name: string | null;
+		email: string | null;
+	}) {
+		const collectionRef = collection(db, 'users');
+		const getUserQuery = query(collectionRef, where('uid', '==', uid));
+
+		const querySnapshot = await getDocs(getUserQuery);
+
+		if (querySnapshot.docs.length > 0) return;
+
+		await addDoc(collection(db, 'users'), {
+			name: name,
+			uid: uid,
+			email: email,
+		});
 	}
 }
